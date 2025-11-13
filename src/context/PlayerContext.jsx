11a33204808631c8dc26fce33f919b79dd1c5b1e
Balasarathi29 <bala2004sarathi@gmail.com>
2025-11-13 11:13:30 +0530

    Add new Changes

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useRef } from "react";
+import { createContext, useRef, useState } from "react";
+import { songsData } from "../assets/spotify-assets/assets/assets";
 
 // Step 1: Create the context
 export const PlayerContext = createContext();
@@ -6,9 +7,43 @@ export const PlayerContext = createContext();
 // Step 2: Create the provider component
  const PlayerContextProvider = (props) => {
   const audioRef = useRef();
+  const seekBg = useRef();
+  const seekBar = useRef();
+
+  const [track , setTrack] = useState(songsData[1])
+  const [playerStatus , setPlayerStatus] = useState(false)
+  const [time , setTime] = useState({
+    currentTime : {
+      second :0,
+      minute : 0
+    },
+    totalTime:{
+      second : 0,
+      minute : 0,
+    }
+  })
+
+  const play = ()=>{
+    audioRef.current.play();
+    setPlayerStatus(true)
+  }
+  const pause = ()=>{
+    audioRef.current.pause();
+    setPlayerStatus(false)
+  }
 
   const contextValue = {
     audioRef,
+    seekBg,
+    seekBar,
+    track,
+    setTrack,
+    playerStatus,
+    setPlayerStatus,
+    time,
+    setTime,
+    play,
+    pause
   };
 
   // Step 3: Use PlayerContext.Provider, not PlayerContextProvider
